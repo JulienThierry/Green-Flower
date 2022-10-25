41309516e470f216ffffb1535c9198e6e94f2e19
Julien Thierry <julienthierrypro@gmail.com>
2022-10-26 01:47:17 +0200

    filter

--- green-flower/src/pages/Products.js
+++ green-flower/src/pages/Products.js
@@ -6,30 +6,53 @@ import { db } from '../utils/firebase.config';
 
 const Products = () => {
     const [productsArray, setProductsArray] = useState([]);
+    const [filter, setFilter] = useState("");
+
+
 
 
     useEffect(() => {
+        // function handleFilter(products) {
+
+        //     products.map((product) => {
+        //         if (product.data().type === filter) {
+        //             return product;
+        //         } else {
+        //             return null;
+        //         }
+        //     })
+        //}
+
         async function fetchData() {
-            await getDocs(collection(db, "products"))
-                .then((res) => setProductsArray(res.docs.map((doc) => ({ ...doc.data(), id: doc.id }))))
+            if (filter) {
+                await getDocs(collection(db, "products"))
+                    .then((res) => setProductsArray(res.docs
+                        .filter((product) => product.data().type === filter)
+                        .map((doc) => ({ ...doc.data(), id: doc.id }))))
+            } else {
+                await getDocs(collection(db, "products"))
+                    .then((res) => setProductsArray(res.docs.map((doc) => ({ ...doc.data(), id: doc.id }))))
+            }
         }
         fetchData();
-    }, [])
+    }, [filter])
 
     return (
         <div>
             <Menu />
             <div className="sort-bar">
                 <ul>
-                    <li>
-
-                    </li>
+                    <li onClick={() => setFilter("bouquets")} >Bouquets</li>
+                    <li>Graines</li>
+                    <li>Fleurs</li>
+                    <li>Offres</li>
+                    <li>Cadeaux</li>
                 </ul>
             </div>
             <div className="products">
                 {productsArray.map((product) => <Card product={product} key={product.id} />)}
             </div>
-        </div>
+        </div >
     );
 };
 
